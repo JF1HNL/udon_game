7a468716b5dac4452d4e030025ec54192f85f96c
JF1HNL <kimossan88@gmail.com>
2019-09-07 21:09:58 +0900

    コンポーネントファイル分けたいよ、、、

---
+++ index.js
@@ -0,0 +1,15 @@
+import  { insta_data } from "./data.js";
+
+let App = new Vue({
+  el: '#udon',
+  data: {
+    page: 1
+  }
+})
+
+let app2 = new Vue({
+  el: '#test',
+  data: {
+    pages: 2
+  }
+})
