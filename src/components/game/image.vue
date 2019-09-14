3cb3f8fc0778757c49da5c00f7392d72a1756f8b
JF1HNL <kimossan88@gmail.com>
2019-09-14 15:32:21 +0900

    imgコンポーネント書いてみた、

--- src/components/game/image.vue
+++ src/components/game/image.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
-
+    <div v-if="display_flag">
+      <img :src="url" @click="udon_click(position)">
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  
+  props: [
+    'display_flag',
+    'url',
+    'position'
+  ],
+  methods: {
+    udon_click: function(position){
+      this.$emit(position)
+    }
+  }
 }
 </script>
--- src/components/game/index.vue
+++ src/components/game/index.vue
@@ -2,13 +2,18 @@
   <div>
     <h1>game</h1>
     <input type="button" value="result" @click="page(PAGE_NAME.result)" >
+    <Img :display_flag="true" :url="PAGE_NAME"/>
   </div>
 </template>
 
 <script>
 import {PAGE_NAME} from '../../const.js'
+import Img from './image.vue'
 
 export default {
+  components: {
+    Img
+  },
   data: function() {
     // PAGE_NAME
     return {
