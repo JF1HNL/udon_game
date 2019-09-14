befbccbe9e0d60df9143567ed919175475b03435
JF1HNL <kimossan88@gmail.com>
2019-09-14 15:55:18 +0900

    dataから情報とってきて、表示できた！

--- src/components/game/index.vue
+++ src/components/game/index.vue
@@ -2,13 +2,15 @@
   <div>
     <h1>game</h1>
     <input type="button" value="result" @click="page(PAGE_NAME.result)" >
-    <Img :display_flag="true" :url="PAGE_NAME"/>
+    <Img v-for='(it, i) in insta_data' :key='i' :display_flag="true" :url="url_maker(it.img)" :position="123"/>
   </div>
 </template>
 
 <script>
 import {PAGE_NAME} from '../../const.js'
 import Img from './image.vue'
+import {insta_data} from './data.js'
+
 
 export default {
   components: {
@@ -17,12 +19,16 @@ export default {
   data: function() {
     // PAGE_NAME
     return {
+      insta_data,
       PAGE_NAME
     }
   },
   methods: {
     page: function(){
       this.$emit(PAGE_NAME.result)
+    },
+    url_maker: function(e){
+      return `https://www.instagram.com/p/${e}/media/?size=m`
     }
   }
 }
