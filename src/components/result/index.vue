8ea848c89f8421a2c62e6aca54fe1be8056c3b2e
JF1HNL <kimossan88@gmail.com>
2019-11-02 16:11:49 +0900

    情報を持ってくることができた！

--- src/components/result/index.vue
+++ src/components/result/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <h1>result</h1>
+    <h1>結果発表！</h1>
+    <ol>
+      <li v-for='(it, i) in result_ary' :key=i>{{ it.menu[0].name }}</li>
+    </ol>
     <input type="button" value="top" @click="page(PAGE_NAME.top)" >
   </div>
 </template>
@@ -9,6 +12,9 @@
 import {PAGE_NAME} from '../../const.js'
 
 export default {
+  props: [
+    'result_ary'
+  ],
   data: function() {
     // PAGE_NAME
     return {
